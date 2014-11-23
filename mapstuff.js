db36d84e4a2dc71dddaedf6f57c684782de0e37d
Sean Ray <sean.ray@wwt.com>
2014-11-23 14:44:55 -0600

    Updates

--- mapstuff.js
+++ mapstuff.js
@@ -10,16 +10,15 @@
     init();
 
     function init() {
-        buildMap('mapquest');
-
         ranger = document.getElementById("ranger");
+
+        buildMap('mapquest');
+        buildMenu();
+        buildMarkersMap();        
 
         ranger.addEventListener("input", function(e) {
             updateLayers(e.target.value);
         });
-        buildMenu();
-
-        buildMarkersMap();
     }
 
     function buildMap(type) {
@@ -41,15 +40,6 @@
         }
     }
 
-    function addMarkers(num) {
-        removeLayers();
-
-        for(var i=0; i < num; i++) {
-            layers.push(new L.marker([center[0]+(i*0.005), center[1]+(i*0.005)]));
-            map.addLayer(layers[i]);
-        }
-    }
-
     function buildMenu() {
         var addMarkersMenuItem = document.getElementById("add-markers");
         var moveCirclesMenuItem = document.getElementById("move-circles");
@@ -63,9 +53,7 @@
         });
     }
 
-    function buildMarkersMap() {        
-
-
+    function buildMarkersMap() {
         addMarkers(ranger.value); 
         updateLayers = addMarkers;   
     }
@@ -82,8 +70,20 @@
         updateLayers = moveCircle;
     }
 
+    function addMarkers(num) {
+        removeLayers();
+
+        for(var i=0; i < num; i++) {
+            layers.push(new L.marker([center[0]+(i*0.005), center[1]+(i*0.005)]));
+            map.addLayer(layers[i]);
+        }
+        map.panTo(layers[layers.length-1].getLatLng());
+    }
+
     function moveCircle(num) {
-        layers[0].setLatLng([center[0]+(num*.005),center[1]+(num*.005)]);
+        var newPt = [center[0]+(num*.005),center[1]+(num*.005)];
+        layers[0].setLatLng(newPt);
+        map.panTo(newPt);
     }
 
     function removeLayers() {
