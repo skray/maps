3d2b5ba6685220e17a2252fb97cd6cf600fde7d7
Sean Ray <sean.ray@wwt.com>
2015-02-08 22:33:22 -0600

    Got livereload working in a somewhat hacky manner

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,28 @@ var concat = require('gulp-concat');
 var path = require('path');
 var del  = require('del');
 var deploy = require('gulp-gh-pages');
+var livereload = require('gulp-livereload');
+
+var EXPRESS_PORT = 8000;
+var EXPRESS_ROOT = 'public';
+var LIVERELOAD_PORT = 35729;
+ 
+// Let's make things more readable by
+// encapsulating each part's setup
+// in its own method
+function startExpress() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')());
+  app.use(express.static(EXPRESS_ROOT));
+  app.listen(EXPRESS_PORT);
+}
+
+var paths = {
+    html: 'src/**/*.html', 
+    js: 'src/**/*.js',
+    less: 'src/**/*.less'
+};
 
 var jsdeps = [
     'node_modules/leaflet/dist/leaflet.js',
@@ -40,8 +62,9 @@ gulp.task('copy-images', ['clean'], function() {
 });
 
 gulp.task('copy-src', ['clean'], function() {
-    return gulp.src(['src/**/*.html', 'src/**/*.js'])
-      .pipe(gulp.dest('public'));
+    return gulp.src([paths.html, paths.js])
+      .pipe(gulp.dest('public'))
+      .pipe(livereload());
 });
 
 gulp.task('copy', ['copy-images', 'copy-src']);
@@ -53,15 +76,23 @@ gulp.task('deploy', ['build'], function () {
 });
 
 gulp.task('less', ['clean'], function () {
-    return gulp.src('src/**/*.less')
+    return gulp.src(paths.less)
       .pipe(less({
         paths: [ path.join(__dirname, 'less', 'includes') ]
       }))
-      .pipe(gulp.dest('public'));
+      .pipe(gulp.dest('public'))
+      .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    startExpress();
+    livereload.listen();
+    gulp.watch([paths.html, paths.js, paths.less], ['build']);
 });
 
 
 gulp.task('publish', ['deploy']);
+gulp.task('dev', ['watch', 'build']);
 gulp.task('build', ['concat', 'less', 'copy'])
 
 gulp.task('default', ['build']);
