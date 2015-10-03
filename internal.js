46c392e053f31a72989a53e7dc136af8fc9981ea
Sean Ray <seankennethray@gmail.com>
2015-10-03 00:06:14 -0500

    Update 2015-10-03T05:06:12.568Z

--- internal.js
+++ internal.js
@@ -32,42 +32,6 @@
 (function(angular) {
 	
 	angular.module('maps')
-		.controller('NavCtrl', NavCtrl);
-
-	function NavCtrl($scope, AuthSvc) {
-
-		var vm = this;
-
-		vm.login = AuthSvc.login;
-		vm.logout = AuthSvc.logout;
-		vm.user = null;
-		vm.flags ={
-			loggedIn: false
-		};
-
-		init();
-
-		function init() {
-			$scope.$on('logged-in', onLoggedIn);
-			$scope.$on('logged-out', onLoggedOut);
-		}
-
-		function onLoggedIn(evt, user) {
-			vm.flags.loggedIn = true;
-			vm.user = user;
-		}
-
-		function onLoggedOut() {
-			vm.flags.loggedIn = false;
-			vm.user = null;
-		}
-
-	}
-
-})(window.angular);
-(function(angular) {
-	
-	angular.module('maps')
 		.service('AuthSvc', AuthSvc);
 
 	function AuthSvc(FIREBASE_REF, $firebaseAuth, $rootScope) {
@@ -128,7 +92,7 @@
 		vm.saveLayer = saveLayer;
 
 		vm.flags = { 
-			editingMapMeta: true,
+			editingMapMeta: false,
 			settingCenterAndZoom: false,
 			canEdit: false
 		};
@@ -279,6 +243,42 @@
 		}
 
 		return createMapList; 
+	}
+
+})(window.angular);
+(function(angular) {
+	
+	angular.module('maps')
+		.controller('NavCtrl', NavCtrl);
+
+	function NavCtrl($scope, AuthSvc) {
+
+		var vm = this;
+
+		vm.login = AuthSvc.login;
+		vm.logout = AuthSvc.logout;
+		vm.user = null;
+		vm.flags ={
+			loggedIn: false
+		};
+
+		init();
+
+		function init() {
+			$scope.$on('logged-in', onLoggedIn);
+			$scope.$on('logged-out', onLoggedOut);
+		}
+
+		function onLoggedIn(evt, user) {
+			vm.flags.loggedIn = true;
+			vm.user = user;
+		}
+
+		function onLoggedOut() {
+			vm.flags.loggedIn = false;
+			vm.user = null;
+		}
+
 	}
 
 })(window.angular);
